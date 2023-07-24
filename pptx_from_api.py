e7d34b9d90248c3e205d9952b8a4dd293d7ef9ed
aong90 <133682003+aong90@users.noreply.github.com>
2023-07-24 09:27:50 -0500

    Update pptx_from_api.py

--- pptx_from_api.py
+++ pptx_from_api.py
@@ -1,75 +1,76 @@
+import os
 import requests
 import json
 from pptx import Presentation
-from pptx.util import Inches
-from typing import List
+from pptx.util import Inches, Pt
 
-# Global constant
-API_BASE = 'https://abcd2.projectabcd.com/api/getinfo.php?id='
+# Declare the API base URL as a global constant
+API_BASE_URL = "https://abcd2.projectabcd.com/api/getinfo.php?id="
 
-def generate_the_pptx(id_list: List[int], sort_order: str = 'ascending') -> None:
-    presentation = Presentation()
+def fetch_data_from_api(id):
+    url = API_BASE_URL + str(id)
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
 
-    for id in id_list:
-        api_url = API_BASE + str(id)
-        #406 error- add header, did not solve 406
-        headers = {
-            'Accept': 'application/json',
-        }
-        response = requests.get(api_url, headers=headers)
-         
-        # Check if request was successful
-        if response.status_code == 200:
-            # Get JSON data from the response
-            data = response.json()
+    # Make API request and check if the response is successful (status code 200):
+    response = requests.get(url, headers=headers)
 
-            if 'data' in data:
-                data = data['data']
-                id = data.get('id', '')
-                name = data.get('name', '')
-                description = data.get('description', '')
-                
-                
-                # Create a new slide
-                slide_layout = presentation.slide_layouts[1]
-                slide = presentation.slides.add_slide(slide_layout)
-
-                # Add title
-                title_placeholder = slide.shapes.title
-                title_placeholder.text = f"ID: {id} - {name}"
-
-                # Add content
-                content_placeholder = slide.shapes.add_textbox(Inches(1), Inches(1.5), Inches(8), Inches(5))
-                content_frame = content_placeholder.text_frame
-                content_frame.text = description
-
-                # Add sorting order
-                if sort_order == 'ascending':
-                    presentation.slides._sldIdLst = sorted(presentation.slides._sldIdLst, key=lambda x: x.idx)
-
-            else:
-                print(f"Data not found for ID {id}")
-        else:
-            print(f"Failed to retrieve data for ID {id}. Error: {response.status_code} - {response.text}")
-
-    if len(presentation.slides) > 0:
-        presentation.save('output.pptx')
+    if response.status_code == 200:
+        return response.json()['data']
     else:
-        print("No valid data retrieved. Presentation not saved.")
+        print(f"Failed to get data from the API for ID {id}. Status code:", response.status_code)
+        return None
 
-# Prompt user to enter IDs
-id_list_input = input("Enter the IDs: ")
-id_list = [int(id.strip()) for id in id_list_input.split(",")]
+def create_slide(prs, data):
+    slide_master = prs.slide_master
+    slide_layout = slide_master.slide_layouts[1]
+    slide = prs.slides.add_slide(slide_layout)
 
-# Call the function with user-specified IDs
-generate_the_pptx(id_list)
+    title_shape = slide.shapes.title
+    title_shape.text = data['name']
 
+    content_shape = slide.placeholders[1]
+    content_text = f"Description: {data['description']}\n"
+    content_text += f"Did You Know: {data.get('did_you_know', '')}\n"
+    content_text += f"Category: {data.get('category', '')}\n"
+    content_text += f"Type: {data.get('type', '')}\n"
+    content_text += f"State Name: {data.get('state_name', '')}\n"
+    content_text += f"Key Words: {data.get('key_words', '')}\n"
+    content_text += f"Image URL: {data.get('image_url', '')}\n"
+    content_text += f"Status: {data.get('status', '')}\n"
+    content_text += f"Notes: {data.get('notes', '')}\n"
+    content_text += f"Book: {data.get('book', '')}\n"
+    content_shape.text = content_text
+    
+    # Add the image to the slide
+    image_filename = data.get('image_url', '')
+    if image_filename:
+        image_path = os.path.join(os.path.dirname(__file__), image_filename)
+        left = Inches(1)
+        top = Inches(2)
+        pic = slide.shapes.add_picture(image_path, left, top)
 
+def main():
+    prs = Presentation()
+    prs.slide_width = Inches(8)
+    prs.slide_height = Inches(11)
 
-'''
-BUGS: 406 issue
-API endpoint and url is correct 
-Header format?  Does api endpoint need additional headers or authentication?
-server issue? is it configured to handle the requested format
-Mod_Security? -open source web application firewall that provides security for web applications-it intercepts
-and inspects requests and responses-if it detects suspicious behavior, it can block the request
+    # Read the ID numbers from 'slide_numbers.txt' file
+    with open('slide_numbers.txt', 'r') as file:
+        ids_str = file.read()
+
+    # Convert the comma-separated numbers into a list of integers
+    ids = [int(id.strip()) for id in ids_str.split(',')]
+
+    for id in ids:
+        data = fetch_data_from_api(id)
+        if data:
+            create_slide(prs, data)
+
+    prs.save("api.pptx")
+    print("PowerPoint file 'api.pptx' created successfully.")
+
+if __name__ == "__main__":
+    main()
